56953b7c5bd3e9033e8bb1ef4dd34e5cf9ed6b3f
Vít Vlašánek <vit.vlasanek@ecmsystem.cz>
2024-03-13 17:36:18 +0100

    Added attributes

---
+++ ORM/Attributes/PrimaryKey.cs
@@ -0,0 +1,7 @@
+﻿namespace Vitvlasanek.Cs2.Project.Backend.ORM.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    internal class PrimaryKey : Attribute
+    {
+    }
+}
